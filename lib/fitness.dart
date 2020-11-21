273f56abaeb166ac4253a96eeabb388391b520e8
Harmanjit Singh <harmanjit140500@gmail.com>
2020-11-21 19:35:19 +0530

    futnes mostly done

--- lib/fitness.dart
+++ lib/fitness.dart
@@ -1,6 +1,7 @@
 import 'package:RAISE/colorFile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 // import 'package:video_player/video_player.dart';
 import 'package:iqplayer/iqplayer.dart';
@@ -26,6 +27,11 @@ class _FitnessScreenState extends State<FitnessScreen> with ColorFile {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.green,
+        child: Icon(FontAwesomeIcons.clock),
+      ),
       backgroundColor: deepBrown,
       appBar: AppBar(
         centerTitle: true,
@@ -54,13 +60,349 @@ class _FitnessScreenState extends State<FitnessScreen> with ColorFile {
               ),
             ),
             Container(
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
+              width: double.maxFinite,
+              height: 300,
               child: ListView(
                 shrinkWrap: true,
-                children: [],
+                children: [
+                  OutlineButton(
+                      padding: EdgeInsets.all(15),
+                      borderSide: BorderSide(color: white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => IQScreen(
+                              title: "Weight Loss for Women",
+                              description: 'Fitness Videos',
+                              videoPlayerController:
+                                  VideoPlayerController.network(
+                                'https://www.youtube.com/watch?v=xViWU57xDk4',
+                              ),
+                              iqTheme: IQTheme(
+                                loadingProgress: SpinKitCircle(
+                                  color: Colors.white,
+                                ),
+                                playButtonColor: Colors.transparent,
+                                videoPlayedColor: Colors.indigo,
+
+                                // playButton: (true) {
+                                //   if (isPlay)
+                                //     return Icon(
+                                //       Icons.pause_circle_filled,
+                                //       color: Colors.red,
+                                //       size: 50,
+                                //     );
+                                //   return Icon(
+                                //     Icons.play_circle_outline,
+                                //     color: Colors.red,
+                                //     size: 50,
+                                //   );
+                                // },
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Weight Loss for Women",
+                            style: TextStyle(fontSize: 20, color: white),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.play_arrow,
+                            color: white,
+                            size: 30,
+                          )
+                        ],
+                      )),
+                  Divider(),
+                  OutlineButton(
+                      padding: EdgeInsets.all(15),
+                      borderSide: BorderSide(color: white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => IQScreen(
+                              title: "Weight Loss for all",
+                              description: 'Fitness Videos',
+                              videoPlayerController:
+                                  VideoPlayerController.network(
+                                'https://www.youtube.com/watch?v=AXZlb-3MMYE',
+                              ),
+                              iqTheme: IQTheme(
+                                loadingProgress: SpinKitCircle(
+                                  color: Colors.white,
+                                ),
+                                playButtonColor: Colors.transparent,
+                                videoPlayedColor: Colors.indigo,
+
+                                // playButton: (true) {
+                                //   if (isPlay)
+                                //     return Icon(
+                                //       Icons.pause_circle_filled,
+                                //       color: Colors.red,
+                                //       size: 50,
+                                //     );
+                                //   return Icon(
+                                //     Icons.play_circle_outline,
+                                //     color: Colors.red,
+                                //     size: 50,
+                                //   );
+                                // },
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Weight Loss for all",
+                            style: TextStyle(fontSize: 20, color: white),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.play_arrow,
+                            color: white,
+                            size: 30,
+                          )
+                        ],
+                      )),
+                  Divider(),
+                  OutlineButton(
+                      padding: EdgeInsets.all(15),
+                      borderSide: BorderSide(color: white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => IQScreen(
+                              title: "Full Body Workout",
+                              description: 'Fitness Videos',
+                              videoPlayerController:
+                                  VideoPlayerController.network(
+                                'https://www.youtube.com/watch?v=ml6cT4AZdqI&feature=youtu.be',
+                              ),
+                              iqTheme: IQTheme(
+                                loadingProgress: SpinKitCircle(
+                                  color: Colors.white,
+                                ),
+                                playButtonColor: Colors.transparent,
+                                videoPlayedColor: Colors.indigo,
+
+                                // playButton: (true) {
+                                //   if (isPlay)
+                                //     return Icon(
+                                //       Icons.pause_circle_filled,
+                                //       color: Colors.red,
+                                //       size: 50,
+                                //     );
+                                //   return Icon(
+                                //     Icons.play_circle_outline,
+                                //     color: Colors.red,
+                                //     size: 50,
+                                //   );
+                                // },
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Full Body Workout",
+                            style: TextStyle(fontSize: 20, color: white),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.play_arrow,
+                            color: white,
+                            size: 30,
+                          )
+                        ],
+                      )),
+                  Divider(),
+                  OutlineButton(
+                      padding: EdgeInsets.all(15),
+                      borderSide: BorderSide(color: white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => IQScreen(
+                              title: "20 min Workouty",
+                              description: 'Fitness Videos',
+                              videoPlayerController:
+                                  VideoPlayerController.network(
+                                'https://www.youtube.com/watch?v=AzV3EA-1-yM&feature=youtu.be',
+                              ),
+                              iqTheme: IQTheme(
+                                loadingProgress: SpinKitCircle(
+                                  color: Colors.white,
+                                ),
+                                playButtonColor: Colors.transparent,
+                                videoPlayedColor: Colors.indigo,
+
+                                // playButton: (true) {
+                                //   if (isPlay)
+                                //     return Icon(
+                                //       Icons.pause_circle_filled,
+                                //       color: Colors.red,
+                                //       size: 50,
+                                //     );
+                                //   return Icon(
+                                //     Icons.play_circle_outline,
+                                //     color: Colors.red,
+                                //     size: 50,
+                                //   );
+                                // },
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "20-min Workout",
+                            style: TextStyle(fontSize: 20, color: white),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.play_arrow,
+                            color: white,
+                            size: 30,
+                          )
+                        ],
+                      )),
+                  Divider(),
+                  OutlineButton(
+                      padding: EdgeInsets.all(15),
+                      borderSide: BorderSide(color: white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => IQScreen(
+                              title: "30-min Kick-Boxing",
+                              description: 'Fitness Videos',
+                              videoPlayerController:
+                                  VideoPlayerController.network(
+                                'hhttps://www.youtube.com/watch?v=hIXMt5CGlIM&feature=youtu.be',
+                              ),
+                              iqTheme: IQTheme(
+                                loadingProgress: SpinKitCircle(
+                                  color: Colors.white,
+                                ),
+                                playButtonColor: Colors.transparent,
+                                videoPlayedColor: Colors.indigo,
+
+                                // playButton: (true) {
+                                //   if (isPlay)
+                                //     return Icon(
+                                //       Icons.pause_circle_filled,
+                                //       color: Colors.red,
+                                //       size: 50,
+                                //     );
+                                //   return Icon(
+                                //     Icons.play_circle_outline,
+                                //     color: Colors.red,
+                                //     size: 50,
+                                //   );
+                                // },
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "30-min Kick-Boxing",
+                            style: TextStyle(fontSize: 20, color: white),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.play_arrow,
+                            color: white,
+                            size: 30,
+                          )
+                        ],
+                      )),
+                  Divider(),
+                  OutlineButton(
+                      padding: EdgeInsets.all(15),
+                      borderSide: BorderSide(color: white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => IQScreen(
+                              title: "Weight Loss for Women",
+                              description: 'Fitness Videos',
+                              videoPlayerController:
+                                  VideoPlayerController.network(
+                                'https://www.youtube.com/watch?v=xViWU57xDk4',
+                              ),
+                              iqTheme: IQTheme(
+                                loadingProgress: SpinKitCircle(
+                                  color: Colors.white,
+                                ),
+                                playButtonColor: Colors.transparent,
+                                videoPlayedColor: Colors.indigo,
+
+                                // playButton: (true) {
+                                //   if (isPlay)
+                                //     return Icon(
+                                //       Icons.pause_circle_filled,
+                                //       color: Colors.red,
+                                //       size: 50,
+                                //     );
+                                //   return Icon(
+                                //     Icons.play_circle_outline,
+                                //     color: Colors.red,
+                                //     size: 50,
+                                //   );
+                                // },
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Temp Workout",
+                            style: TextStyle(fontSize: 20, color: white),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.play_arrow,
+                            color: white,
+                            size: 30,
+                          )
+                        ],
+                      )),
+                ],
               ),
             ),
             Container(
-              color: Colors.white,
+              margin: EdgeInsets.only(top: 15),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(0)),
+              // padding: EdgeInsets.all(10),
               child: Column(
                 children: [
                   Container(
@@ -77,44 +419,6 @@ class _FitnessScreenState extends State<FitnessScreen> with ColorFile {
                   ),
                 ],
               ),
-            ),
-            RaisedButton(
-              child: Text('Open IQPlayer'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => IQScreen(
-                      title: "hello",
-                      description: 'Simple video as a demo video',
-                      videoPlayerController: VideoPlayerController.network(
-                        'https://d11b76aq44vj33.cloudfront.net/media/720/video/5def7824adbbc.mp4',
-                      ),
-                      iqTheme: IQTheme(
-                        loadingProgress: SpinKitCircle(
-                          color: Colors.white,
-                        ),
-                        playButtonColor: Colors.transparent,
-                        videoPlayedColor: Colors.indigo,
-
-                        // playButton: (true) {
-                        //   if (isPlay)
-                        //     return Icon(
-                        //       Icons.pause_circle_filled,
-                        //       color: Colors.red,
-                        //       size: 50,
-                        //     );
-                        //   return Icon(
-                        //     Icons.play_circle_outline,
-                        //     color: Colors.red,
-                        //     size: 50,
-                        //   );
-                        // },
-                      ),
-                    ),
-                  ),
-                );
-              },
             ),
           ]),
         ),
