5abfff16241a04e12f0f01114667a68ad4d22ebf
Harmanjit Singh <harmanjit140500@gmail.com>
2020-11-21 19:41:38 +0530

    health started

--- lib/health.dart
+++ lib/health.dart
@@ -1,15 +1,31 @@
+import 'package:RAISE/colorFile.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HealthPage extends StatefulWidget {
   @override
   _HealthPageState createState() => _HealthPageState();
 }
 
-class _HealthPageState extends State<HealthPage> {
+class _HealthPageState extends State<HealthPage> with ColorFile {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+      child: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Available Self-Tests",
+              style: GoogleFonts.poppins(
+                  textStyle:
+                      TextStyle(color: Colors.pink, fontWeight: FontWeight.bold),
+                  fontSize: 25),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
